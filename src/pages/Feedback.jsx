4fcde1ecc84269963346d855f9c3541ff2d4aeaa
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-07-28 23:18:39 -0300

    fix(feedback): fixing a bug on feedback page

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -17,7 +17,9 @@ export default function Feedback() {
       <h1 className="nameOnFeedback">{name}</h1>
       <h2 className="scoreOnFeedback">{score}</h2>
       {result >= 80 ? <h2>CONGRATULATIONS! YOU ROCK IT!!!</h2> : ''}
-      {result < 80 && result >= 60 ? <h2>You did a great job!</h2> : <h2>Could be better. Try again!</h2>}
+      {result < 80 && result >= 60 ? <h2>You did a great job!</h2> : ''}
+      {result < 60 ? <h2>Could be better. Try again!</h2> : ''}
+
       <div className="rightOfTotal">
         <h2 className="rights">{rightAnswers} </h2>
         <h2 className="outOf">{numberOfQuestions}</h2>
